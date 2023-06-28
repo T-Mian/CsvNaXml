e568598591a8bed405819fc4a4a16b09ec984b76
TomaszMianecki <6973773-TomaszMianecki@users.noreply.replit.com>
2023-06-28 08:13:57 +0000

    Opis programu i poprawa wyglądu

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """
 Tomasz Mianecki
-versja_alfa_001
+Program do zmiany plików csv zawierajacych dane dla kart produktu
+na pliki xml do użytku dla strony Globus Lighting wersja ang/fra
+versja_alfa_1.2
 """
 #importy
 import xml.etree.ElementTree as ET
@@ -10,7 +12,8 @@ import random
 #flaga bool
 flaga_ik = False
 flaga_dedykowana_lista = False
-# listy do urzycia 
+
+# listy do urzycia
 lista_kolumn_alfa = [
   "kod", "kodbarwy", "p", "flux", "sterowanie", "IP", "pled"
 ]
@@ -21,6 +24,7 @@ lista_kolumn_beta = [
   "kod", "kodbarwy", "p", "flux", "sterowanie", "IP", "IK", "pled"
 ]
 lista_kolumn_dedykowana = []
+
 # dict z dodatkami do danych
 dict_dorostki_danych = {
   "kod": 0,
@@ -54,12 +58,12 @@ dict_dorostki_danych = {
   "wymiary": "mm"
 }
 
-# input od operatora 
+# input od operatora
 nazwa_pliku_csv = input("Podaj nazwe pliku csv:")
-
 nazwa_ok_Q = input(
   "\n Czy nazwa rodziny jest zawarta w pliku csv enter 't'> Tak, lub 'n'> dla Nie : "
 )
+
 # logika do obsługi inputu operatora
 if nazwa_ok_Q.upper() == "T":
   rodzina = nazwa_pliku_csv[:-5]
@@ -75,6 +79,7 @@ elif nazwa_ok_Q.upper() == "Q":
 else:
   print("Wprowadzono nierozpoznawalną komende :", nazwa_ok_Q)
   exit()
+
 
 # definicjia dla odczytu danych z pliku
 def odczyt_danych_csv(plik_csv, list_zapisu):
@@ -98,9 +103,11 @@ def odczyt_danych_csv(plik_csv, list_zapisu):
       komponent = ""
   list_wynik.pop(0)
   print("Funkcja odczyt_danych_csv wykonana poprawnie ", list_zapisu, "\n")
-  print("lista wynikowa",list_wynik)
+  print("lista wynikowa", list_wynik)
   return list_wynik
-# definicjia: tworzenie dedykowanej listy z inputu od operatora 
+
+
+# definicjia: tworzenie dedykowanej listy z inputu od operatora
 def dedekowane_dane_listy():
   lista = ["kod", "kodbarwy", "p"]
   print(
@@ -111,10 +118,12 @@ def dedekowane_dane_listy():
   )
   input_dla_listy = input("Enter :")
   lista_return = input_dla_listy.split("/")
-  print("lista_return",lista_return)
+  print("lista_return", lista_return)
   lista.extend(lista_return)
-  print("lista dedykowana",lista)
+  print("lista dedykowana", lista)
   return lista
+
+
 # definicjia: tworzenie pliku xml
 def struktura_xml(lista):
   versja_Xml_input = input(
@@ -179,6 +188,7 @@ def struktura_xml(lista):
   with open(nazwa_pliku_xml + ".xml", "wb") as f:
     f.write(b_xml)
 
+
 # input określający jaką liste z kolumnami wybrać
 print("\n Standard danych do pobrania z csv to 'Nova' czyli :\n",
       lista_kolumn_beta, "\n")
@@ -187,7 +197,7 @@ inpuT_funkcje = input(
 )
 print("wybrana opcja :", inpuT_funkcje.upper())
 
-# logika obsługi wybranych opcji 
+# logika obsługi wybranych opcji
 if inpuT_funkcje.upper() == "N":
   lista_wynikowa_odczytu = odczyt_danych_csv(nazwa_pliku_csv,
                                              lista_kolumn_beta)
@@ -196,12 +206,13 @@ elif inpuT_funkcje.upper() == "B":
   lista_wynikowa_odczytu = odczyt_danych_csv(nazwa_pliku_csv,
                                              lista_kolumn_alfa)
 elif inpuT_funkcje.upper() == "L":
-  flaga_dedykowana_lista=True
-  lista_kolumn_dedykowana=dedekowane_dane_listy()
+  flaga_dedykowana_lista = True
+  lista_kolumn_dedykowana = dedekowane_dane_listy()
   lista_wynikowa_odczytu = odczyt_danych_csv(nazwa_pliku_csv,
                                              lista_kolumn_dedykowana)
 
 # wywołanie funkcji tworzącej xml
 struktura_xml(lista_wynikowa_odczytu)
-# info o zakończeniu działania 
+
+# info o zakończeniu działania
 print("koniec programu")
