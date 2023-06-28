c5489860e3cc04c12324fa3e80459588b7e45b3d
TomaszMianecki <6973773-TomaszMianecki@users.noreply.replit.com>
2023-06-28 08:10:03 +0000

    Zapis pomocniczych komentarzy

--- main.py
+++ main.py
@@ -7,20 +7,21 @@ import xml.etree.ElementTree as ET
 from csv import DictReader
 import random
 
+#flaga bool
 flaga_ik = False
 flaga_dedykowana_lista = False
+# listy do urzycia 
 lista_kolumn_alfa = [
   "kod", "kodbarwy", "p", "flux", "sterowanie", "IP", "pled"
 ]
 lista_subElementów_alfa = [
   "KOD", "RACCT", "MOC", "FLUX", "CONTROL", "IX", "ROZ"
 ]
-
 lista_kolumn_beta = [
   "kod", "kodbarwy", "p", "flux", "sterowanie", "IP", "IK", "pled"
 ]
 lista_kolumn_dedykowana = []
-
+# dict z dodatkami do danych
 dict_dorostki_danych = {
   "kod": 0,
   "rodzina": 0,
@@ -53,11 +54,13 @@ dict_dorostki_danych = {
   "wymiary": "mm"
 }
 
-# input od operatora okreslenie nazw
+# input od operatora 
 nazwa_pliku_csv = input("Podaj nazwe pliku csv:")
+
 nazwa_ok_Q = input(
   "\n Czy nazwa rodziny jest zawarta w pliku csv enter 't'> Tak, lub 'n'> dla Nie : "
 )
+# logika do obsługi inputu operatora
 if nazwa_ok_Q.upper() == "T":
   rodzina = nazwa_pliku_csv[:-5]
   rodzina = rodzina.upper()
@@ -97,7 +100,7 @@ def odczyt_danych_csv(plik_csv, list_zapisu):
   print("Funkcja odczyt_danych_csv wykonana poprawnie ", list_zapisu, "\n")
   print("lista wynikowa",list_wynik)
   return list_wynik
-
+# definicjia: tworzenie dedykowanej listy z inputu od operatora 
 def dedekowane_dane_listy():
   lista = ["kod", "kodbarwy", "p"]
   print(
@@ -112,7 +115,7 @@ def dedekowane_dane_listy():
   lista.extend(lista_return)
   print("lista dedykowana",lista)
   return lista
-
+# definicjia: tworzenie pliku xml
 def struktura_xml(lista):
   versja_Xml_input = input(
     "Podaj prosze wersję(liczba naturalna dodatnia od 0 do 2 147 483 647) pliku xml lub wpisz R=> dlarandomowego generowania: "
@@ -176,7 +179,7 @@ def struktura_xml(lista):
   with open(nazwa_pliku_xml + ".xml", "wb") as f:
     f.write(b_xml)
 
-
+# input określający jaką liste z kolumnami wybrać
 print("\n Standard danych do pobrania z csv to 'Nova' czyli :\n",
       lista_kolumn_beta, "\n")
 inpuT_funkcje = input(
@@ -184,6 +187,7 @@ inpuT_funkcje = input(
 )
 print("wybrana opcja :", inpuT_funkcje.upper())
 
+# logika obsługi wybranych opcji 
 if inpuT_funkcje.upper() == "N":
   lista_wynikowa_odczytu = odczyt_danych_csv(nazwa_pliku_csv,
                                              lista_kolumn_beta)
@@ -197,7 +201,7 @@ elif inpuT_funkcje.upper() == "L":
   lista_wynikowa_odczytu = odczyt_danych_csv(nazwa_pliku_csv,
                                              lista_kolumn_dedykowana)
 
-
-print(lista_wynikowa_odczytu)
+# wywołanie funkcji tworzącej xml
 struktura_xml(lista_wynikowa_odczytu)
+# info o zakończeniu działania 
 print("koniec programu")
